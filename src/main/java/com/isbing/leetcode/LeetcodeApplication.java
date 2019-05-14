b6135bb814bc3c69ee7fe7b73e75959182a62a2a
songbing <404863881@qq.com>
2019-05-14 17:24:27 +0800

    算法题库

---
+++ src/main/java/com/isbing/leetcode/LeetcodeApplication.java
@@ -0,0 +1,16 @@
+package com.isbing.leetcode;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class LeetcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetcodeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/isbing/leetcode/coding/LeetCode125.java
@@ -0,0 +1,9 @@
+package com.isbing.leetcode.coding;
+
+/**
+ * Created by song bing
+ * Created time 2019/5/14 17:23
+ */
+public class LeetCode125 {
+
+}
