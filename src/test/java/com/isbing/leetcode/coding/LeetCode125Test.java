883c4d188867f05a8cd90e1cb885695d5f8c952e
songbing <404863881@qq.com>
2019-06-27 17:56:27 +0800

    第一题

--- src/test/java/com/isbing/leetcode/coding/LeetCode125Test.java
+++ src/test/java/com/isbing/leetcode/coding/LeetCode125Test.java
@@ -1,38 +1,16 @@
----
-title: LeetCode125-判断回文串
-date: 2019-06-01 20:00:00
-categories: 算法小白的刷题之路
-tags: LeetCode
----
+package com.isbing.leetcode.coding;
 
-## 题目
-
-给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
-
-说明：本题中，我们将空字符串定义为有效的回文串。
-
-示例 1:
-
-输入: "A man, a plan, a canal: Panama"
-
-输出: true
-
-示例 2:
-
-输入: "race a car"
-
-输出: false
-
-## 分析
-
-1. 题目要求 只考虑字母和数字字符。所以其他不合法的字符都可以看做不存在一样
-2. 这里可以考虑 前后指针移动 前后指针指向的字符比较
-3. 时间复杂度O(N) 空间复杂度为O(1)
-
-## 解答
-
-````java
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by song bing
+ * Created time 2019/6/27 17:34
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class LeetCode125Test {
 
 	@Test
@@ -86,14 +64,3 @@ public class LeetCode125Test {
 	}
 
 }
-
-````
-
-
-
-
-
-
-
-
-
